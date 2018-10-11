cd21a9e2ed79a8f9a25758f660fd5667eb7cf748
Krzysztof Krzepicki <Krzysztof.Krzepicki@gmail.com>
2018-10-11 11:20:51 +0200

    Warehouse Search module - complete v0.1

--- application/controllers/Inventory.php
+++ application/controllers/Inventory.php
@@ -10,25 +10,29 @@ class Inventory extends CI_Controller {
     public function search(){
         $ItemCode = $this->input->post('ItemCode') ? $this->input->post('ItemCode') : '';
         $ItemCatalogNumber = $this->input->post('ItemCatalogNumber') ? $this->input->post('ItemCatalogNumber') : '';
-        $ItemName = $this->input->post('ItemName') ? $this->input->post('ItemName') : '';
-        if($ItemCode=='' && $ItemCatalogNumber=='' && $ItemName=='')
+        $Warehouse = $this->input->post('Warehouse') ? $this->input->post('Warehouse') : '';
+        if($ItemCode=='' && $ItemCatalogNumber=='' && $Warehouse=='')
         { 
             $this->session->set_flashdata('alert', array( 'color'=>'warning', 'title'=>'Błąd formularza', 'content'=>'Należy wypełnić przynajmniej jedno pole lub pobrać wszystkie rekordy.'));
             $this->load->template('inventory/ItemSearchForm');
         }else{
-            $this->load->model('dataTable_model');
-            $this->load->model('DataTable_model');
-            $data['dataTable']=$this->DataTable_model->get_all_items();
+            $ItemCode=$this->input->post('ItemCode');
+            $ItemCatalogNumber=$this->input->post('ItemCatalogNumber');
+            $Warehouse=$this->input->post('Warehouse');
+            
+            $this->load->model('Inventory_model');
+            $data['dataTable'] = $this->Inventory_model->get_items('searchForm',$ItemCatalogNumber,$ItemCode,$Warehouse);
+            
             $this->load->template('Inventory/ItemList',$data);
+            
         }
         
 
     }
     
     public function dynamic_table(){
-        $this->load->model('dataTable_model');
-        $this->load->model('DataTable_model');
-        $data['dataTable']=$this->DataTable_model->get_all_items();
+        $this->load->model('Inventory_model');
+        $data['dataTable'] = $this->Inventory_model->get_items('all');
         $this->load->template('Inventory/ItemList',$data);
     }
     
