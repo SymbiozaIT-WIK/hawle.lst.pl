bfd6272bc1ff8ea4be5cfb0d9da19b2befb84738
Krzysztof Krzepicki <Krzysztof.Krzepicki@gmail.com>
2018-10-11 14:42:16 +0200

    views/order/index
    poprawka metody przesyłania zmiennej na post z formularza

--- application/views/Order/Index.php
+++ application/views/Order/Index.php
@@ -1,6 +1,6 @@
 <div class="container">
     <div class="col-md-4">
-        <form action="<?php echo site_url('Order/Create') ?>">
+        <form action="<?php echo site_url('Order/Create') ?>" method="post">
             <button id="submit" class="btn btn-primary btn-block">Stwórz Zamówienie</button>
         </form>
     </div>
