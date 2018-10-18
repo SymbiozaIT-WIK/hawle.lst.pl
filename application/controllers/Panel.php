f9df3a57d80f8760fca1d02304b2e4c725773bd6
Krzysztof Krzepicki <Krzysztof.Krzepicki@gmail.com>
2018-10-18 08:55:32 +0200

    panele

--- application/controllers/Panel.php
+++ application/controllers/Panel.php
@@ -4,7 +4,38 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 class Panel extends CI_Controller {
     
 	public function index(){
-        $this->load->template('panelAdmin');
+        
+        $usertype= $this->session->userdata('usertype');
+        switch ($usertype){
+                
+            case 'A':
+                $this->load->template('panelAdmin');
+                break;
+                
+            case 'R':
+                $this->load->template('panelReg');
+                break;
+                
+            case 'C':
+                $this->load->template('panelCust');
+                break;
+            default:
+                $alert=array(
+                    'title' => 'Nieznany typ użytkownika.',
+                    'content' => 'Skontaktuj się z administratorem.',
+                    'color' => 'danger'
+                );
+                $this->session->set_flashdata('alert',$alert);
+                redirect('');
+                break;
+                
+        }
+        
+        
+        
+        
+        
+        
     }
 }
 ?>
